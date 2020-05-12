1bbb56785653495996ab03be05dba8b38f912ed4
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-05-12 20:00:13 +0200

    more weeks

--- make_trigrams.sh
+++ make_trigrams.sh
@@ -3,11 +3,9 @@
 for file in weekly/*.text
 do
     week=$(echo $file| tr -d '[:alpha:][:punct:]')
+    
     ./trigram $file  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -n > "./trigrams/$week.text"
-#    ./bigram  $file  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -nr | head -50 | sort -n >  "./bigrams/$week.text"
+    ./bigram  $file  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -n | >  "./bigrams/$week.text"
 
 done    
 
-#    ./bigram  $file  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -nr | head -50 | sort -n >  "./bigrams/$week.text"
-
-#done    
