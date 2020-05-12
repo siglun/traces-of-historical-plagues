c9d0d987de98a229130bd47568121e6cd56eab5d
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-05-12 23:15:05 +0200

    The result of a rebuild

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ gnuplot < plot.gp
 gnuplot < plot_weekly.gp
 
 # ./make_trigrams.sh
-./trigrams_to_bar_diagram > dia.ms
-groff -ms  -p  -P-pa4 -Tps parameters.ms text.ms dia.ms parameters2.ms > bar_diagram.ps
+#./trigrams_to_bar_diagram > dia.ms
+# groff -ms  -p  -P-pa4 -Tps parameters.ms text.ms dia.ms parameters2.ms > bar_diagram.ps
+groff -ms  -p  -P-pa4 -Tps parameters.ms text.ms > bar_diagram.ps
 ps2pdf  bar_diagram.ps
