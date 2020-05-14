cd3074ede0c2b49ad66746fa1d6c74e5ea6c9243
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-05-14 18:23:58 +0200

    A graph w  last word with trigrams cholera i

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# ./cholera_i
+
 gnuplot < plot.gp
 gnuplot < plot_weekly.gp
 
-# ./make_trigrams.sh
+# ./make_trigrams.s
+./plot_cholera_in > cholera.ms
 ./trigrams_to_bar_diagram > dia.ms
-groff -ms  -p  -P-pa4 -Tps parameters.ms text.ms dia.ms  > bar_diagram.ps
-groff -ms  -p  -P-pa4 -Tps parameters.ms text.ms > paper_only.ps
+groff -ms -s  -p  -P-pa4 -Tps parameters.ms text.ms parameters2.ms cholera.ms dia.ms > bar_diagram.ps
+groff -ms  -p  -P-pa4 -Tps parameters.ms text.ms parameters2.ms cholera.ms > paper_only.ps
 ps2pdf  bar_diagram.ps
 ps2pdf  paper_only.ps
