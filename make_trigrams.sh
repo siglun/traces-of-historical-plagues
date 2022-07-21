f7bfcd9ba7a60f2989160aef3cc1e964f36eecc8
Sigfrid Lundberg <siggelundberg@gmail.com>
2022-07-21 13:45:38 +0200

    (re)introducing spell check

--- make_trigrams.sh
+++ make_trigrams.sh
@@ -4,8 +4,8 @@ for file in weekly/*.text
 do
     week=$(echo $file| tr -d '[:alpha:][:punct:]')
     
-    ./trigram $file  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -n > "./trigrams/$week.text"
-    ./bigram  $file  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -n  >  "./bigrams/$week.text"
+    ./filter.pl < $file | ./trigram | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -n > "./trigrams/$week.text"
+    ./filter.pl < $file | ./bigram  | egrep '(epidemi)|(kolera)|(cholera)' | egrep -v '[[:punct:][:digit:]]+' | iconv -tISO-8859-1 -fUTF-8  | sort -d | uniq -c | sort -n | ./filter.pl >  "./bigrams/$week.text"
 
 done    
 
